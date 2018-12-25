c452d4bd44d917be24c8664c8821581d29f4ca1c
Peter <peter.r.herman@gmail.com>
2018-12-24 19:30:57 -0700

    shortened range to 24 hours, added temp

--- test.py
+++ test.py
@@ -13,13 +13,19 @@ forecast_df['date'] = forecast_df['startTime'].str[5:10]
 forecast_df['hour'] = forecast_df['startTime'].str[11:13]
 forecast_df['time'] = forecast_df['date'] + ', ' + forecast_df['hour'] + ':00'
 forecast_df['Wind'] = forecast_df['windSpeed'].str.rstrip(' mph').astype(int)
+forecast_24 = forecast_df.loc[0:24,:]
+
 fig, axis = plt.subplots()
-bar_width = 0.2
-axis.bar(forecast_df.index, forecast_df['Wind'],bar_width,
-                label='Wind Speed')
+bar_width = 0.5
+bars1 = axis.bar(forecast_24.index, forecast_24['Wind'],bar_width,
+                alpha = 0.5, label='Wind Speed')
+bars2 = axis.bar(forecast_24.index+bar_width, forecast_24['temperature'], bar_width,
+         alpha = 0.5,label = 'Temperature')
+axis.legend()
 axis.set_xlabel('Date, Time')
 axis.set_ylabel('Wind Speed (MPH)')
 axis.set_title('Hourly Wind Speed Forecast')
-axis.set_xticks(forecast_df.index + bar_width / 2)
-axis.set_xticklabels(forecast_df['time'])
+axis.set_xticks(forecast_24.index + bar_width / 2)
+axis.set_xticklabels(forecast_24['hour'])
+fig.tight_layout()
 plt.show()
